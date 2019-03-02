e4b1ee550e8ef2cdc65d8a4447bff7670dc4b633
Valentin Lécart <Valentin@La-Voie-Lactee.local>
2019-03-02 17:53:57 +0100

    range

---
+++ ex21/ft_range.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Valentin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/02 17:43:52 by Valentin          #+#    #+#             */
+/*   Updated: 2019/03/02 17:53:37 by Valentin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int		*ft_range(int min, int max)
+{
+	int		*tab;
+	int		i;
+
+	i = 0;
+	if (!(tab = (int*)malloc(sizeof(int) * (max - min))))
+		return (NULL);
+	while (min < max)
+	{
+		tab[i] = min;
+		min++;
+		i++;
+	}
+	return (tab);
+}
